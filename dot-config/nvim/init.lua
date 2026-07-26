338e4ca0fcbe00ea907aeabf9c853888c44b330d
lthnh <imr3ng4l@gmail.com>
2026-07-26 17:17:37 +0700

    add redraw autocmd when leaving buffer and change lua file format

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -30,84 +30,106 @@ map('n', '<leader>pu', vim.pack.update)
 local filetype_customize_group = vim.api.nvim_create_augroup('customize_by_filetype', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
-    group = filetype_customize_group,
-    pattern = { 'systemverilog' , 'verilog' },
-    callback = function()
-        vim.bo.tabstop = 2
-        vim.bo.shiftwidth = 2
-        map('i', "'", "'", { buffer = true })
-    end,
+  group = filetype_customize_group,
+  pattern = 'lua',
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+  end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-    group = filetype_customize_group,
-    pattern = 'markdown',
-    callback = function()
-        vim.wo.conceallevel= 1
-        vim.wo.wrap = true
-        vim.wo.linebreak = true
-        vim.b.minipairs_disable = true
-    end,
+  group = filetype_customize_group,
+  pattern = { 'systemverilog' , 'verilog' },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    map('i', "'", "'", { buffer = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = filetype_customize_group,
+  pattern = 'markdown',
+  callback = function()
+    vim.wo.conceallevel= 1
+    vim.wo.wrap = true
+    vim.wo.linebreak = true
+    map('i', "`", "`", { buffer = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufLeave', {
+  group = vim.api.nvim_create_augroup('Redraw Graphics', { clear = true }),
+  pattern = '*',
+  callback = function()
+    vim.cmd('redraw')
+  end,
 })
 
 vim.api.nvim_create_autocmd({'VimEnter', 'VimLeave'}, {
-    group = vim.api.nvim_create_augroup('Plugins cleanup', { clear = true }),
-    pattern = '*',
-    callback = function()
-        local unused_plugins = vim.iter(vim.pack.get())
-                                :filter(function(x) return not x.active end)
-                                :map(function(x) return x.spec.name end)
-                                :totable()
-        if next(unused_plugins) ~= nil then
-            vim.pack.del(unused_plugins)
-        end
+  group = vim.api.nvim_create_augroup('Plugins cleanup', { clear = true }),
+  pattern = '*',
+  callback = function()
+    local unused_plugins = vim.iter(vim.pack.get())
+                :filter(function(x) return not x.active end)
+                :map(function(x) return x.spec.name end)
+                :totable()
+    if next(unused_plugins) ~= nil then
+      vim.pack.del(unused_plugins)
     end
+  end
 })
 
 vim.pack.add({
-    { src = 'https://github.com/ellisonleao/gruvbox.nvim' },
-    { src = 'https://github.com/mitander/flume.nvim' },
-    {
-        src = 'https://github.com/nvim-mini/mini.icons',
-        version = 'stable',
-    },
-    { src = 'https://github.com/nvim-mini/mini.statusline' },
-    { src = 'https://github.com/stevearc/oil.nvim' },
-    { src = 'https://github.com/nvim-mini/mini.pairs' },
-    { src = 'https://github.com/nvim-mini/mini.pick' },
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = 'https://github.com/mason-org/mason.nvim' },
-    { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-    {
-        src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-        version = 'main',
-    },
-    {
-        src = 'https://github.com/saghen/blink.compat',
-        version = vim.version.range('2.*')
-    },
-    {
-        src = 'https://github.com/saghen/blink.cmp',
-        version = 'v1',
-    },
-    {
-        src = 'https://github.com/obsidian-nvim/obsidian.nvim',
-        version = 'main'
-    },
-    { src = 'https://github.com/meanderingprogrammer/render-markdown.nvim' },
-    { src = 'https://github.com/nvim-orgmode/orgmode' },
+  { src = 'https://github.com/ellisonleao/gruvbox.nvim' },
+  { src = 'https://github.com/mitander/flume.nvim' },
+  {
+    src = 'https://github.com/rose-pine/neovim',
+    name = 'rose-pine'
+  },
+  {
+    src = 'https://github.com/nvim-mini/mini.icons',
+    version = 'stable',
+  },
+  { src = 'https://github.com/nvim-mini/mini.statusline' },
+  { src = 'https://github.com/stevearc/oil.nvim' },
+  { src = 'https://github.com/nvim-mini/mini.pairs' },
+  { src = 'https://github.com/nvim-mini/mini.pick' },
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/mason-org/mason.nvim' },
+  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+  {
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
+  },
+  {
+    src = 'https://github.com/saghen/blink.cmp',
+    version = 'v1',
+  },
+  {
+    src = 'https://github.com/obsidian-nvim/obsidian.nvim',
+    version = 'main'
+  },
+  { src = 'https://github.com/meanderingprogrammer/render-markdown.nvim' },
+  { src = 'https://github.com/nvim-orgmode/orgmode' },
 })
 
 --require('gruvbox').setup()
 --vim.cmd.colorscheme('gruvbox')
 require('flume').setup()
 vim.cmd.colorscheme('flume')
+--require('rose-pine').setup({
+  --variant = 'moon',
+  --dark_variant = 'main'
+--})
+--vim.cmd.colorscheme('rose-pine')
 vim.cmd(':hi signcolumn guibg=NONE')
 --vim.cmd(':hi WinSeparator guibg=NONE')
 
 local status, _ = pcall(require, 'mini.statusline')
 if not status then
-    vim.cmd(':hi statusline guibg=NONE')
+  vim.cmd(':hi statusline guibg=NONE')
 end
 
 require('mini.icons').setup()
@@ -126,7 +148,7 @@ local language_server =  {'lua_ls', 'slang_server', 'clangd', 'rust_analyzer', '
 
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = language_server
+  ensure_installed = language_server
 })
 
 vim.lsp.enable(language_server)
@@ -134,102 +156,122 @@ map('n', '<leader>lf', vim.lsp.buf.format)
 
 -- default auto complete
 --vim.api.nvim_create_autocmd('LspAttach', {
-    --callback = function(ev)
-            --local client = vim.lsp.get_client_by_id(ev.data.client_id)
-            --if client == nil then
-               --print('client is nil')
-               --return
-            --end
-            --if client:supports_method('textDocument/completion') then
-                --vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-            --end
-    --end,
+  --callback = function(ev)
+      --local client = vim.lsp.get_client_by_id(ev.data.client_id)
+      --if client == nil then
+         --print('client is nil')
+         --return
+      --end
+      --if client:supports_method('textDocument/completion') then
+        --vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      --end
+  --end,
 --})
 --vim.cmd('set completeopt+=noselect')
 
 
 require('nvim-treesitter').install({
-    'c', 'cpp', 'cmake', 'bash', 'systemverilog', 'vhdl', 'rust', 'yaml', 'markdown'
+  'c', 'cpp', 'cmake', 'bash', 'systemverilog', 'vhdl', 'rust', 'yaml', 'markdown'
 })
 vim.api.nvim_create_autocmd('PackChanged', {
-    callback = function(event)
-        if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
-            vim.schedule(function()
-                vim.notify('Updating treesitter parsers and queries.')
-                vim.cmd('TSUpdate')
-            end)
-        end
-    end,
+  callback = function(event)
+    if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
+      vim.schedule(function()
+        vim.notify('Updating treesitter parsers and queries.')
+        vim.cmd('TSUpdate')
+      end)
+    end
+  end,
 })
 
-require('blink.compat').setup()
 require('blink.cmp').setup({
-    completion = {
-      menu = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                return kind_icon
-              end,
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                return hl
-              end,
-            },
-            kind = {
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                return hl
-              end,
-            }
-          }
-        }
+  completion = {
+    menu = {
+    draw = {
+      components = {
+      kind_icon = {
+        text = function(ctx)
+        local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+        return kind_icon
+        end,
+        -- (optional) use highlights from mini.icons
+        highlight = function(ctx)
+        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+        return hl
+        end,
       },
-    list = {
-        selection = {
-            preselect = false
-        },
+      kind = {
+        -- (optional) use highlights from mini.icons
+        highlight = function(ctx)
+        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+        return hl
+        end,
+      }
+      }
+    }
     },
-    documentation = { auto_show = true },
+  list = {
+    selection = {
+      preselect = false
     },
-    keymap = {
-        ['<CR>'] = { 'accept', 'fallback' },
-    },
-    sources = {
-        per_filetype = {
-            lua = { 'lsp', 'path', 'snippets' },
-            markdown = { 'path', 'lsp' },
-            tex = { 'path', 'lsp', 'snippets' }
-        }
-    },
-    enabled = function()
-        local disabled_filetypes = { 'text' }
-        return not vim.tbl_contains(disabled_filetypes, vim.bo.filetype) and vim.bo.buftype ~= 'prompt'
-    end
+  },
+  documentation = { auto_show = true },
+  },
+  keymap = {
+    ['<CR>'] = { 'accept', 'fallback' },
+  },
+  sources = {
+    per_filetype = {
+      lua = { 'lsp', 'path', 'snippets' },
+      markdown = { 'path', 'lsp' },
+      tex = { 'path', 'lsp', 'snippets' },
+      toml = { 'path', 'lsp' }
+    }
+  },
+  enabled = function()
+    local disabled_filetypes = { 'text' }
+    return not vim.tbl_contains(disabled_filetypes, vim.bo.filetype) and vim.bo.buftype ~= 'prompt'
+  end
 })
 
 require('obsidian').setup({
-    legacy_commands = false,
-    picker = {
-        name = 'mini.pick'
+  legacy_commands = false,
+  picker = {
+    name = 'mini.pick'
+  },
+  ui = {
+    enable = false
+  },
+  link = {
+    style = 'wiki',
+    format = 'shortest',
+    auto_update = true
+  },
+  checkbox = {
+    enabled = true,
+    create_new = false
+  },
+  workspaces = {
+    {
+      name = 'Embedded',
+      path = '~/data/knowledge-base/Embedded/'
     },
-    workspaces = {
-        {
-            name = 'Embedded',
-            path = '~/data/knowledge-base/Embedded/'
-        },
+    {
+      name = 'IC Design',
+      path = '~/data/knowledge-base/IC Design/'
     },
+    {
+      name = 'Linux Journey',
+      path = '~/data/knowledge-base/Linux Journey/'
+    },
+  },
 })
 
 require('render-markdown').setup()
 
 require('orgmode').setup({
-    org_agenda_files = '~/data/knowledge-base/orgfiles/**/*',
-    org_default_notes_file = '~/data/knowledge-base/orgfiles/refile.org',
+  org_agenda_files = '~/data/knowledge-base/orgfiles/**/*',
+  org_default_notes_file = '~/data/knowledge-base/orgfiles/refile.org',
 })
 vim.lsp.enable('org')
 
