032a8ed35e1dbcfdab4eabf2621c31ddd5374f20
lthnh <imr3ng4l@gmail.com>
2026-08-09 12:38:26 +0700

    change obsidian.nvim plugin version to 3.*.* (stable)

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -102,7 +102,7 @@ vim.pack.add({
   },
   {
     src = 'https://github.com/obsidian-nvim/obsidian.nvim',
-    version = 'main'
+    version = vim.version.range('3.*.*')
   },
   { src = 'https://github.com/meanderingprogrammer/render-markdown.nvim' },
   { src = 'https://github.com/nvim-orgmode/orgmode' },
